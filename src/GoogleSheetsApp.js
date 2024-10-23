788e49dbf597012e7a097fc3b0f543223e7e7cc4
osmanesad <byesad@gmail.com>
2024-10-23 04:24:27 +0300

    Update GoogleSheetsApp.js

--- src/GoogleSheetsApp.js
+++ src/GoogleSheetsApp.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 
-const SHEET_ID = 'XXXXX';
+const SHEET_ID = '1oCG5VsgXfzoroQ4XBEaRKW59rFignOGitzHd54ZmQDM';
 
-const API_KEY = 'XXXXX';
+const API_KEY = 'AIzaSyAXKShQUgtM2l2t7BxZehlKzQ5uYWT8ciI';
 
 const GoogleSheetsApp = () => {
   const [data, setData] = useState([]);
